6c93e52399ad5e7d8aee7c46ec1f717454605870
bst <rg7525007471@gmail.com>
2024-02-19 14:48:13 +0530

    first commit

--- src/API.js
+++ src/API.js
@@ -1,3 +1,4 @@
 // export const API = "https://admin-backend-api-wqpq.onrender.com/api";
 // export const API = "https://ecom-backend-three.vercel.app/api";
-export const API = "http://localhost:4300/api";
+export const API = "https://admin-back-end.onrender.com/api";
+// export const API = "http://localhost:4300/api";
